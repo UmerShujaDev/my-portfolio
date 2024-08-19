66bdcebe21a87a896cc90e064cd3d425f658cdc7
umerShujaDev <umershuja12@gmail.com>
2024-08-20 00:39:01 +0500

    reset form after email send

--- components/contact.tsx
+++ components/contact.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React from "react";
+import React, { useRef } from "react";
 import SectionHeading from "./section-heading";
 import { motion } from "framer-motion";
 import { useSectionInView } from "@/lib/hooks";
@@ -10,6 +10,7 @@ import toast from "react-hot-toast";
 
 export default function Contact() {
   const { ref } = useSectionInView("Contact");
+  const formRef: any = useRef(null);
 
   return (
     <motion.section
@@ -40,16 +41,23 @@ export default function Contact() {
       </p>
 
       <form
+        ref={formRef}
         className="mt-10 flex flex-col dark:text-black"
-        action={async (formData) => {
+        action={async (formData: any) => {
           const { data, error } = await sendEmail(formData);
 
           if (error) {
             toast.error(error);
             return;
           }
-
+          
           toast.success("Email sent successfully!");
+
+           // Reset the form fields
+          // formData.get("senderEmail").value = "";
+          // formData.get("message").value = "";
+          formRef.current.reset();
+
         }}
       >
         <input
